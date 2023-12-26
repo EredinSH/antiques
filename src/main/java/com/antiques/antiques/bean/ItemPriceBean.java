951318c92432b76e2130d288b54e4b4cdf21f19f
EredinSH <cehakm@gmail.com>
2023-12-26 20:11:28 +0100

    commit 21

--- src/main/java/com/antiques/antiques/bean/ItemPriceBean.java
+++ src/main/java/com/antiques/antiques/bean/ItemPriceBean.java
@@ -4,26 +4,47 @@ import com.antiques.antiques.ui.item.ItemForm;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
 
 @ManagedBean
 public class ItemPriceBean {
 
-    @Inject
-    ItemForm itemForm;
+    @ManagedProperty(value = "#{currentPriceBean}")
+    CurrentPriceBean currentPriceBean;
 
-    public void validatePrice(Object value) throws ValidatorException {
-        double previousPrice = itemForm.getItem().getPrice();
-        double newPrice = (Double) value;
+    private Double newPrice;
+
+    public CurrentPriceBean getCurrentPriceBean() {
+        return currentPriceBean;
+    }
+
+    public void setCurrentPriceBean(CurrentPriceBean currentPriceBean) {
+        this.currentPriceBean = currentPriceBean;
+    }
+
+    public Double getNewPrice() {
+        return newPrice;
+    }
+
+    public void setNewPrice(Double newPrice) {
+        this.newPrice = newPrice;
+    }
+
+    public void validatePrice(Double value) throws ValidatorException {
+        double previousPrice = currentPriceBean.getCurrenPrice();
+        double newPrice = value;
         if(newPrice <= previousPrice) {
             FacesMessage message = new FacesMessage("Proponowana cena musi być wyższa od aktualnej!");
             throw new ValidatorException(message);
         }
-        itemForm.getItem().setPrice(newPrice);
+        currentPriceBean.setCurrenPrice(value);
     }
 
     public String doValidate() {
+        FacesContext context = FacesContext.getCurrentInstance();
         return "items";
     }
 }
